91e512a269321c5cb2fd033427b2ca73d34864a8
coding-liki <coding1liki@gmail.com>
2017-04-24 23:47:18 +0300

    скачан Cimg

--- perceptron/perceptron.cpp
+++ perceptron/perceptron.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "perceptron.h"
+#include <time.h>
 int Rand_Variants(int* a, int b)
 {
 	int *v;
@@ -32,6 +33,7 @@ int main()
 	int b[9];
 	int shag;
 	int corb=0, cosb=0;
+	srand(time(NULL));
 	for(int i=0;i<9;i++) a[i]=1;
 	while(1)
 	{
@@ -52,9 +54,13 @@ int main()
 		a[shag-1]=2;
 		P1.New_State(a);
 		P1.Count();
+
+			printf("Состояние  --\n 	");
 		for(int k=0;k<9;k++)
 		{
 			b[k]=P1.c_stats[k];
+			printf("%d ",b[k] );
+			if(k%3==2) printf("\n 	");
 		}
 		int klok=Rand_Variants(b,9);
 		if(klok!=-1)
